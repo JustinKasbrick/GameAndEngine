0900895d7e2925ac72792ff7bf3fe5a10f90ff6b
Justin Kasbrick <jusam311@hotmail.ca>
2013-11-27 16:05:51 -0600

    splash screen and main menu screen

---
+++ src/com/jkgames/GameAndEngine/MainMenuActivity.java
@@ -0,0 +1,231 @@
+package com.jkgames.GameAndEngine;
+
+
+import android.graphics.Color;
+import android.view.KeyEvent;
+import android.widget.Toast;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.menu.MenuScene;
+import org.anddev.andengine.entity.scene.menu.animator.SlideMenuAnimator;
+import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.TextMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.decorator.ColorMenuItemDecorator;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.font.FontFactory;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainMenuActivity extends BaseGameActivity implements MenuScene.IOnMenuItemClickListener {
+
+    private static final int CAMERA_WIDTH = 480;
+    private static final int CAMERA_HEIGHT = 320;
+    protected static final int MENU_ABOUT = 0;
+    protected static final int MENU_QUIT = MENU_ABOUT + 1;
+    protected static final int MENU_PLAY = 100;
+    protected static final int MENU_SCORES = MENU_PLAY + 1;
+    protected static final int MENU_OPTIONS = MENU_SCORES + 1;
+    protected static final int MENU_HELP = MENU_OPTIONS + 1;
+
+    protected Camera mCamera;
+    protected Scene mMainScene;
+    private BitmapTextureAtlas mMenuBackTexture;
+    private TextureRegion mMenuBackTextureRegion;
+    protected MenuScene mStaticMenuScene, mPopUpMenuScene;
+    private BitmapTextureAtlas mPopUpTexture;
+    private BitmapTextureAtlas mFontTexture;
+    private Font mFont;
+    protected TextureRegion mPopUpAboutTextureRegion;
+    protected TextureRegion mPopUpQuitTextureRegion;
+    protected TextureRegion mMenuPlayTextureRegion;
+    protected TextureRegion mMenuScoresTextureRegion;
+    protected TextureRegion mMenuOptionsTextureRegion;
+    protected TextureRegion mMenuHelpTextureRegion;
+    private boolean popupDisplayed;
+
+    @Override
+    public Engine onLoadEngine() {
+        this.mCamera = new Camera(0, 0, CAMERA_WIDTH,
+                CAMERA_HEIGHT);
+        return new Engine(new EngineOptions(true,
+                EngineOptions.ScreenOrientation.LANDSCAPE,
+                new RatioResolutionPolicy(CAMERA_WIDTH,
+                        CAMERA_HEIGHT), this.mCamera));
+    }
+
+    @Override
+    public void onLoadResources() {
+        /* Load Font/Textures. */
+//        this.mFontTexture = new BitmapTextureAtlas(256, 256,
+//                TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+//        FontFactory.setAssetBasePath("font/");
+//        this.mFont = FontFactory.createFromAsset(this.mFontTexture,
+//                this, "Flubber.ttf", 32, true, Color.RED);
+//        this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+//        this.mEngine.getFontManager().loadFont(this.mFont);
+        this.mMenuBackTexture = new BitmapTextureAtlas(512, 512,
+                TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        this.mMenuBackTextureRegion =
+                BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mMenuBackTexture,
+                        this, "MainMenuBk.png", 0, 0);
+        this.mEngine.getTextureManager().loadTexture(this.mMenuBackTexture);
+//        this.mPopUpTexture = new BitmapTextureAtlas(512, 512,
+//                TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+//        this.mPopUpAboutTextureRegion =
+//                BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mPopUpTexture,
+//                        this, "gfx/MainMenu/About_button.png", 0, 0);
+//        this.mPopUpQuitTextureRegion =
+//                BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mPopUpTexture,
+//                        this, "gfx/MainMenu/Quit_button.png", 0, 50);
+//        this.mEngine.getTextureManager().loadTexture(this.mPopUpTexture);
+        popupDisplayed = false;
+    }
+
+    @Override
+    public Scene onLoadScene() {
+        this.mEngine.registerUpdateHandler(new FPSLogger());
+        this.createStaticMenuScene();
+        this.createPopUpMenuScene();
+        /* Center the background on the camera. */
+        final int centerX = ( CAMERA_WIDTH -
+                this.mMenuBackTextureRegion.getWidth()) / 2;
+        final int centerY = (CAMERA_HEIGHT -
+                this.mMenuBackTextureRegion.getHeight()) /
+                2;
+        this.mMainScene = new Scene();
+        /* Add the background and static menu */
+        final Sprite menuBack = new Sprite(centerX,
+                centerY, this.mMenuBackTextureRegion);
+        mMainScene.getLastChild().attachChild(menuBack);
+        mMainScene.setChildScene(mStaticMenuScene);
+        return this.mMainScene;
+    }
+
+    @Override
+    public void onLoadComplete() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean onKeyDown(final int pKeyCode,
+                             final KeyEvent pEvent) {
+        if(pKeyCode == KeyEvent.KEYCODE_MENU &&
+                pEvent.getAction() == KeyEvent.ACTION_DOWN) {
+            if(popupDisplayed) {
+                /* Remove the menu and reset it. */
+                this.mPopUpMenuScene.back();
+                mMainScene.setChildScene(mStaticMenuScene);
+                popupDisplayed = false;
+            } else {
+                /* Attach the menu. */
+                this.mMainScene.setChildScene(
+                        this.mPopUpMenuScene, false, true, true);
+                popupDisplayed = true;
+            }
+            return true;
+        } else {
+            return super.onKeyDown(pKeyCode, pEvent);
+        }
+    }
+
+    @Override
+    public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem, float pMenuItemLocalX, float pMenuItemLocalY) {
+            switch(pMenuItem.getID()) {
+                case MENU_ABOUT:
+                    Toast.makeText(MainMenuActivity.this,
+                            "About selected",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                case MENU_QUIT:
+                    /* End Activity. */
+                    this.finish();
+                    return true;
+                case MENU_PLAY:
+                    Toast.makeText(MainMenuActivity.this,
+                            "Play selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                case MENU_SCORES:
+                    Toast.makeText(MainMenuActivity.this,
+                            "Scores selected",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                case MENU_OPTIONS:
+                    Toast.makeText(MainMenuActivity.this,
+                            "Options selected",
+                            Toast.LENGTH_SHORT).show();
+                    return true;
+                case MENU_HELP:
+                    Toast.makeText(MainMenuActivity.this,
+                            "Help selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+                    return false;
+            }
+    }
+
+    // ===========================================================
+// Methods
+// ===========================================================
+    protected void createStaticMenuScene() {
+        this.mStaticMenuScene = new MenuScene(this.mCamera);
+        final IMenuItem playMenuItem = new ColorMenuItemDecorator(
+                new TextMenuItem(MENU_PLAY, mFont, "Play Game"),
+                0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
+        playMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mStaticMenuScene.addMenuItem(playMenuItem);
+        final IMenuItem scoresMenuItem =
+                new ColorMenuItemDecorator(
+                        new TextMenuItem(MENU_SCORES, mFont, "Scores"),
+                        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
+        scoresMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mStaticMenuScene.addMenuItem(scoresMenuItem);
+        final IMenuItem optionsMenuItem =
+                new ColorMenuItemDecorator(
+                        new TextMenuItem(MENU_OPTIONS, mFont, "Options"),
+                        0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
+        optionsMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mStaticMenuScene.addMenuItem(optionsMenuItem);
+        final IMenuItem helpMenuItem = new ColorMenuItemDecorator(
+                new TextMenuItem(MENU_HELP, mFont, "Help"),
+                0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f);
+        helpMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mStaticMenuScene.addMenuItem(helpMenuItem);
+        this.mStaticMenuScene.buildAnimations();
+        this.mStaticMenuScene.setBackgroundEnabled(false);
+        this.mStaticMenuScene.setOnMenuItemClickListener(this);
+    }
+    protected void createPopUpMenuScene() {
+        this.mPopUpMenuScene = new MenuScene(this.mCamera);
+        final SpriteMenuItem aboutMenuItem =
+                new SpriteMenuItem(MENU_ABOUT,
+                        this.mPopUpAboutTextureRegion);
+        aboutMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mPopUpMenuScene.addMenuItem(aboutMenuItem);
+        final SpriteMenuItem quitMenuItem = new SpriteMenuItem(
+                MENU_QUIT, this.mPopUpQuitTextureRegion);
+        quitMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA,
+                GL10.GL_ONE_MINUS_SRC_ALPHA);
+        this.mPopUpMenuScene.addMenuItem(quitMenuItem);
+        this.mPopUpMenuScene.setMenuAnimator(
+                new SlideMenuAnimator());
+        this.mPopUpMenuScene.buildAnimations();
+        this.mPopUpMenuScene.setBackgroundEnabled(false);
+        this.mPopUpMenuScene.setOnMenuItemClickListener(this);
+    }
+}
---
+++ src/com/jkgames/GameAndEngine/MyActivity.java
@@ -0,0 +1,81 @@
+package com.jkgames.GameAndEngine;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.scene.background.SpriteBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.bitmap.BitmapTexture;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+public class MyActivity extends BaseGameActivity {
+    // ===========================================================
+    // Constants
+    // ===========================================================
+    private static final int CAMERA_WIDTH = 480;
+    private static final int CAMERA_HEIGHT = 320;
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+    private Camera mCamera;
+    private BitmapTextureAtlas mTexture;
+    private TextureRegion mSplashTextureRegion;
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+
+    @Override
+    public Engine onLoadEngine() {
+        this.mCamera = new Camera(0, 0, CAMERA_WIDTH,
+                CAMERA_HEIGHT);
+        return new Engine(new EngineOptions(true,
+                EngineOptions.ScreenOrientation.LANDSCAPE,
+                new FillResolutionPolicy(),
+                this.mCamera));
+    }
+
+    @Override
+    public void onLoadResources() {
+        this.mTexture = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        this.mSplashTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mTexture, this,
+                "glBackground.png", 0, 0);
+        this.mEngine.getTextureManager().loadTexture(this.mTexture);
+    }
+
+    @Override
+    public Scene onLoadScene() {
+        this.mEngine.registerUpdateHandler(new FPSLogger());
+        final Scene scene = new Scene();
+        /* Center the splash on the camera. */
+        final int centerX =
+                (CAMERA_WIDTH - this.mSplashTextureRegion.getWidth()) / 2;
+        final int centerY =
+                (CAMERA_HEIGHT -
+                        this.mSplashTextureRegion.getHeight()) / 2;
+        /* Create the sprite and add it to the scene. */
+        final Sprite splash = new Sprite(centerX,
+                centerY, this.mSplashTextureRegion);
+        scene.setBackground(new SpriteBackground(splash));
+
+        return scene;
+    }
+
+    @Override
+    public void onLoadComplete() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
